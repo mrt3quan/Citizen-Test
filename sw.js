5f6e4ee204bc83529e09a6d2ecf6d3ffe0e88131
mrt3quan <44070368+mrt3quan@users.noreply.github.com>
2026-08-26 17:44:29 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = "civic-test-app-v14";
+const ASSETS = [
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./mascot-writing.png",
+  "./mascot-trophy.png",
+  "./mascot-book.png",
+  "./mascot-thumbsup.png",
+  "./icon-version.png",
+  "./icon-mystate.png",
+  "./icon-practicelist.png",
+  "./icon-myquestions.png",
+  "./icon-editanswers.png",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).catch(() => {})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const url = event.request.url;
+  if (url.includes("unpkg.com")) {
+    event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
+    return;
+  }
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
+});
